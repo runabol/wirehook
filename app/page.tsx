13b0988440657e6ec2dbc6ffffdeb7741e860386
Arik Cohen <me@arikcohen.com>
2024-12-28 14:39:57 -0500

    Fix webhook url generation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-import { redirect } from "next/navigation";
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-export default async function Home() {
-  const id = Math.random().toString(36).substring(2, 10);
-  redirect(`/hook/${id}`);
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(`/hook/${Math.random().toString(36).substring(2, 10)}`);
+  }, [router]);
+
+  return null;
 }
